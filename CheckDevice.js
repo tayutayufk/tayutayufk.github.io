5ea844cfe9961c638aa8d5a1287f75d008a9f279
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-24 23:24:48 +0900

    touch test 19

--- CheckDevice.js
+++ CheckDevice.js
@@ -9,3 +9,14 @@ if(ua.indexOf("android") !== -1 || ua.indexOf("ios") !== -1 || ua.indexOf("ipad"
 }
 document.write(Device)
 
+function  FitToDevice(){
+    target = document.getElementById("Device");
+    target.innerHTML = window.navigator.userAgent.toLowerCase();
+    //gameInstance.SendMessage("Main Camera", 'setSmartPhoneMode');
+    //document.write("FUCK");
+    //unityInstance.SendMessage("Main Camera", 'setSmartPhoneMode')
+    ua = window.navigator.userAgent.toLowerCase();
+    if(ua.indexOf("android") !== -1 || ua.indexOf("ios") !== -1 || ua.indexOf("ipad") !== -1){
+        gameInstance.SendMessage("MainCamera", 'setSmartPhoneMode')
+    }
+}
