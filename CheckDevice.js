270ea8c5602c05d73b4a8ece360890e3c40cef9a
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-24 15:07:59 +0900

    touch test 16

--- CheckDevice.js
+++ CheckDevice.js
@@ -2,9 +2,9 @@ ua = window.navigator.userAgent.toLowerCase();
 var Device = "PC";
 if(ua.indexOf("android") !== -1 || ua.indexOf("ios") !== -1 || ua.indexOf("ipad") !== -1){
     Device = "SmartPhone";
-    gameInstance.SendMessage("MainCamera", 'setSmartPhoneMode')
+    //gameInstance.SendMessage("MainCamera", 'setSmartPhoneMode')
 }else{
     Device = "Desktop";
-    gameInstance.SendMessage("MainCamera",  'setSmartPhoneMode')
+    //gameInstance.SendMessage("MainCamera",  'setSmartPhoneMode')
 }
 document.write(Device)
