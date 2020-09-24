1582d67ba4e917f2cc82d9fb608d7a79e4deeec1
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-24 13:46:43 +0900

    Update CheckDevice.js

--- CheckDevice.js
+++ CheckDevice.js
@@ -2,7 +2,7 @@ ua = window.navigator.userAgent.toLowerCase();
 var Device = "PC";
 if(ua.indexOf("android") !== -1 || ua.indexOf("ios") !== -1 || ua.indexOf("ipad") !== -1){
     Device = "SmartPhone";
-    unityInstance.SendMessage('Main Camera', 'setSmartPhoneMode')
+    gameInstance.SendMessage("MainCamera", 'setSmartPhoneMode')
 }else{
     Device = "Desktop";
     gameInstance.SendMessage("MainCamera",  'setSmartPhoneMode')
