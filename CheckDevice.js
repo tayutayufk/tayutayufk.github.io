9f6a9c7955b86f5a20ec7dfb48aec6e27aeece13
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-24 13:22:54 +0900

    Update CheckDevice.js

--- CheckDevice.js
+++ CheckDevice.js
@@ -3,6 +3,8 @@ document.write(window.navigator.userAgent.toLowerCase());
 ua = window.navigator.userAgent.toLowerCase();
 var Device = "PC";
 if(ua.indexOf("android") !== -1 || ua.indexOf("ios") !== -1 || ua.indexOf("ipad") !== -1){
-    Device = "SmatrPhone";
+    Device = "SmartPhone";
+}else{
+    Device = "PC";
 }
 document.write(Device)
