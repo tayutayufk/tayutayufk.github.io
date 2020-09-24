76fe017e0440e2597cd2e2f6150efda19ebeed53
tayutayufk <takayasu1j202h19@gmail.com>
2020-09-24 13:24:13 +0900

    Update CheckDevice.js

--- CheckDevice.js
+++ CheckDevice.js
@@ -1,9 +1,10 @@
-document.write(window.navigator.userAgent.toLowerCase());
+//document.write(window.navigator.userAgent.toLowerCase());
 
 ua = window.navigator.userAgent.toLowerCase();
 var Device = "PC";
 if(ua.indexOf("android") !== -1 || ua.indexOf("ios") !== -1 || ua.indexOf("ipad") !== -1){
     Device = "SmartPhone";
+    unityInstance.SendMessage('Main Camera', 'setSmartPhoneMode')
 }else{
     Device = "PC";
 }
