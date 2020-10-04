8f86b74b491d75289159c65226417268b92b096b
tayutayufk <takayasu1j202h19@gmail.com>
2020-10-04 23:30:06 +0900

    10/04

--- button.js
+++ button.js
@@ -1,3 +1,6 @@
 function onClickButton(arg){
     gameInstance.SendMessage('GameDirector',arg);
 }
+function ManiRobot(arg,motor,power){
+    gameInstance.SendMessage('GameDirector',arg,motor,power);
+}
---
+++ editor.js
@@ -0,0 +1,42 @@
+var editor;
+    require.config({ paths: { 'vs': 'node_modules/monaco-editor/min/vs' }});
+    require(['vs/editor/editor.main'], function() {
+        editor = monaco.editor.create(document.getElementById('container'), {
+        value: [].join('\n'),
+        language: 'python',
+        theme: "vs-dark"   
+    });
+});
+
+function outf(text) { 
+    var mypre = document.getElementById("output"); 
+    mypre.innerHTML = mypre.innerHTML + text; 
+} 
+function builtinRead(x) {
+    if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
+            throw "File not found: '" + x + "'";
+    return Sk.builtinFiles["files"][x];
+}
+
+function runit() { 
+   var prog = editor.getValue();
+   var mypre = document.getElementById("output"); 
+   mypre.innerHTML = ''; 
+   Sk.pre = "output";
+   Sk.configure({output:outf, read:builtinRead}); 
+   (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
+   var myPromise = Sk.misceval.asyncToPromise(function() {
+       return Sk.importMainWithBody("<stdin>", false, prog, true);
+   });
+   myPromise.then(function(mod) {
+       console.log('success');
+   },
+       function(err) {
+       console.log(err.toString());
+   });
+} 
+Sk.onAfterImport = function(library) {
+    switch(library) {
+        
+    }
+  }
