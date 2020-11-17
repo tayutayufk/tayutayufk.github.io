9af1bfabc8c069f6f778e57968e630b2672fc14e
tayutayufk <takayasu1j202h19@gmail.com>
2020-11-17 23:10:59 +0900

    login

--- app.py
+++ app.py
@@ -7,81 +7,77 @@ from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = b'haissaiviavbdaivb'
-
+session_sub = {'mail':'','pwd':'','login':'','warn':''}
 
 def check_login():
-    if 'mail' in session and 'pwd' in session and 'login' in session:
-        if session['login'] == 'True':
+    if 'mail' in session_sub and 'pwd' in session_sub and 'login' in session_sub:
+        if session_sub['login'] == 'True':
             return True 
     return False
 
 def check_premium():
     if check_login():
-        data = db.serch_fromMail(session['mail'])#SQLからデータを取得
+        data = db.serch_fromMail(session_sub['mail'])#SQLからデータを取得
         if data[0][2] == "pro":
             return True
     return False
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    if 'warn' not in session:
-        session['warn'] = ''
+    if 'warn' not in session_sub:
+        session_sub['warn'] = ''
 
     if(request.method == "POST"):
-        session['login'] = 'True'
-        return redirect(url_for('index'))   
-        """
+        session_sub['login'] = 'True'
         if 'mail' in request.form and 'pwd' in request.form:
             data = db.serch_fromMail(request.form["mail"])#SQLからデータを取得
             print(data[0][0])
             if data[0][1] == request.form["pwd"]:
                 print("correct pwd")
-                session['mail'] = request.form["mail"]
-                session['pwd'] = request.form["pwd"]
-                session['login'] = 'True'
+                session_sub['mail'] = request.form["mail"]
+                session_sub['pwd'] = request.form["pwd"]
+                session_sub['login'] = 'True'
                 print(data[0][0])
-                session['warn'] = ''
-                return redirect(url_for('index'))   
+                session_sub['warn'] = ''
+                return redirect(url_for('index',session_sub=session_sub))   
             else:
-                session['warn'] = 'unmatch'
-                print(session['warn'])
-                return redirect(url_for('login'))    
-        """
+                session_sub['warn'] = 'unmatch'
+                print(session_sub['warn'])
+                return redirect(url_for('login',session_sub=session_sub))    
     else:
-
-        return render_template("login.html")
+        return render_template("login.html",session_sub=session_sub)
 
 @app.route('/logout')
 def logout():
-    session['login'] = 'False'
-    return redirect(url_for('index'))   
+    session_sub['login'] = 'False'
+    return redirect(url_for('index',session_sub=session_sub))   
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-    if 'warn' not in session:
-        session['warn'] = ''
+    if 'warn' not in session_sub:
+        session_sub['warn'] = ''
     if(request.method == "POST"):
         if 'mail' in request.form and 'pwd' in request.form and 'pwdconf' in request.form:
             if request.form['pwd'] == request.form['pwdconf']:#confirmの確認
                 if db.insert(request.form['mail'],request.form['pwd'],"free"):#メール重複の確認
-                    session['mail'] = request.form["mail"]
-                    session['pwd'] = request.form["pwd"]
-                    session['login'] = 'True'
-                    session['warn'] = ''
-                    return redirect(url_for('index'))
+                    session_sub['mail'] = request.form["mail"]
+                    session_sub['pwd'] = request.form["pwd"]
+                    session_sub['login'] = 'True'
+                    session_sub['warn'] = ''
+                    return redirect(url_for('index',session_sub=session_sub))
                 else:
-                    session['warn'] = 'overlapping'
-                    print(session['warn'])
-                    return redirect(url_for('register'))
+                    session_sub['warn'] = 'overlapping'
+                    print(session_sub['warn'])
+                    return redirect(url_for('register',session_sub=session_sub))
             else:
-                session['warn'] = 'pwdmismatch'
-                print(session['warn'])
-                return redirect(url_for('register'))
+                session_sub['warn'] = 'pwdmismatch'
+                print(session_sub['warn'])
+                return redirect(url_for('register',session_sub=session_sub))
         else:
-            return redirect(url_for('register'))
+            return redirect(url_for('register',session_sub=session_sub))
 
     else:
-        return render_template("register.html")
+        return render_template("register.html",session_sub=session_sub)
 
 #PWA
 @app.route("/manifest.json")
@@ -100,21 +96,11 @@ def favicon():
 #main
 @app.route("/",methods=["GET","POST"]) 
 def index():
-    if check_login():
-        state = "login"
-    else:
-        state="logout"
-
-    return render_template("index.html",state=state)
+    return render_template("index.html",session_sub=session_sub)
 
 @app.route("/index.html")
 def index_sub():
-    if check_login():
-        state = "login"
-    else:
-        state="logout"
-
-    return render_template("index.html",state=state)
+    return render_template("index.html",session_sub=session_sub)
 
 
 @app.route("/craft.html", methods=['GET']) 
@@ -146,15 +132,15 @@ def craft():
             Sensor = ['Sensor',[['CS','Color Sensor','False'],['RF','RangeFinder','True']]]
 
         parts = [Basic,Wheel,Motor,Sensor]
-        return render_template("craft.html",parts=parts)
+        return render_template("craft.html",parts=parts,session_sub=session_sub)
     else:
-        return redirect('login')
+        return redirect(url_for('login',session_sub=session_sub))    
 @app.route("/mission.html", methods=['POST', 'GET']) 
 def mission():
     if check_login():
-        return render_template("mission.html")
+        return render_template("mission.html",session_sub=session_sub)
     else:
-        return redirect('login')
+        return redirect(url_for('login',session_sub=session_sub))    
 
 @app.route("/missionSelect.html", methods=['POST', 'GET']) 
 def missionSelect():
@@ -190,7 +176,7 @@ def missionSelect():
         missions.append(Maze)
         return render_template("missionSelect.html",missions = missions)
     else:
-        return redirect('login')
+        return redirect(url_for('login',session_sub=session_sub))    
 
 
 @app.route("/program.html", methods=['POST', 'GET']) 
@@ -198,9 +184,9 @@ def program():
     if check_login():
         return render_template("program.html")
     else:
-        return redirect('login')
+        return redirect(url_for('login',session_sub=session_sub))    
 
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=8000,debug=True ,threaded=True)
+    app.run(debug=True ,threaded=True)
