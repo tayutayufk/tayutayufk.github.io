a2732d8b6c76567078d886ac7193d4ef66d05cd8
tayutayufk <takayasu1j202h19@gmail.com>
2020-12-29 02:10:31 +0900

    a

--- app.py
+++ app.py
@@ -171,8 +171,8 @@ def favicon():
 def index():
     if 'login' not in session:
         init_session()
-    session['login'] = 'True'
-    session['ver'] = 'pro'
+    #session['login'] = 'True'
+    #session['ver'] = 'pro'
     return render_template("index.html")
 
 @app.route("/index.html")
